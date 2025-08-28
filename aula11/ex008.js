9eaa73bcedabbc564ca0f649add4c1a9f73ad02e
railtonmororo <railtonmororo@gmail.com>
2025-08-28 10:33:24 -0300

    criei o exercício 08

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+ var vel = 50.5
+ console.log(`A velocidade média do seu carro é ${vel}km/h`)
+ if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO')
+ }
+ console.log('Dirija sempre utilizando o cinto de segurança.')
